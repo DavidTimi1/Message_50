566553992e61bb6f212c927e92e07c43807e1c45
David <duwagbale07@gmail.com>
2025-04-07 01:13:23 +0100

    Feat (Custom Loader), Fix (offline/dormant user data persistence)

--- src/contexts.jsx
+++ src/contexts.jsx
@@ -5,13 +5,24 @@ import { generateKeyPair } from './app/crypt.js';
 
 import axiosInstance from "./auth/axiosInstance";
 import { DBrestart } from "./db.jsx";
+import CustomLoader from "./components/Loading.jsx";
 
 export const UserContext = createContext(null);
 
 
 // AuthProvider Component
 export const UserProvider = ({ children, devData }) => {
-	const [userData, setUserData] = useState({});
+	const [userData, setUserData] = useState(() => {
+        const data = localStorage.getItem('userdata');
+		let parsedData;
+		try {
+			parsedData = data? JSON.parse(data) : {};
+		} catch {
+			parsedData = {}
+		}
+        return {...parsedData, reload: reloadUserData};
+    });
+
     const { auth } = useAuth();
 
 	const loadDataUrl = apiHost + "/chat/api/user/me";
@@ -21,7 +32,7 @@ export const UserProvider = ({ children, devData }) => {
 
 		if (!auth && !DevMode) return 
 
-		if (auth) {
+		if (auth && !userData.username) {
 			axiosInstance.get(loadDataUrl)
 			.then( res => {
 				const {dp, public_key} = res.data
@@ -30,23 +41,55 @@ export const UserProvider = ({ children, devData }) => {
 					setUserKeyPair()
 				}
 
+				localStorage.setItem('userdata', JSON.stringify(res.data));
 				setUserData({...res.data, dp: dp, reload: reloadUserData})
 			})
 			.catch((error) => {
 				console.error('Error Loading Data:', error);
+				setUserData({error: error.response ?? 'network'});
 			});
 
-		} else {
+		} else if (!auth) {
 			DevMode && setUserData(devData);
 
 		}
 
 		return () => ignore = true;
-	}, [auth]);
+	}, [auth, userData]);
 
     return (
         <UserContext.Provider value={userData}>
-            {children}
+			{
+				userData.error?
+					<div className="max flex mid-align" style={{justifyContent: "center"}}>
+						{
+							userData.error === 'network'?
+								<div className="max flex-col mid-align gap-4 p-4" style={{justifyContent: "center"}}>
+									<span style={{fontSize: "50px"}}> ⚠ </span>
+									<p>
+										<b> No Internet ! </b>
+									</p>
+								</div>
+							:
+								<div className="p-2 br-5" style={{backgroundColor: "pink", border: "1px solid salmon", color: "red"}}>
+									Oops!😥 this seems bad <br />
+									But no worries, our team is already fixing it 😁
+								</div>
+						}
+					</div>
+					:
+				userData.username?
+					children
+					:
+					<div className="max flex-col mid-align gap-4 p-4" style={{justifyContent: "center"}}>
+						<CustomLoader />
+						<p>
+							<b> Slow Network </b> - Fetching user data is taking longer than usual <br></br>
+							<em> Try checking your internet connection </em>
+						</p>
+					</div>
+
+			}
         </UserContext.Provider>
     );
 
@@ -54,7 +97,7 @@ export const UserProvider = ({ children, devData }) => {
 		if (auth) {
 			axiosInstance.get(loadDataUrl)
 			.then( res => {
-				console.log(res.data)
+				localStorage.setItem('userdata', JSON.stringify(res.data));
 				setUserData({...res.data, reload: reloadUserData})
 			})
 			.catch((error) => {
