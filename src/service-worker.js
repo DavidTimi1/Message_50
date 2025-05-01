b514f203f4f7af285e91b8c2ee9f186aaed2cdae
David <duwagbale07@gmail.com>
2025-05-01 23:58:50 +0100

    Feat (service worker): Add service worker for offline support

---
+++ src/service-worker.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'message50-cache-v1';
+const urlsToCache = [
+    '/',
+    '/manifest.json',
+    '/favicon.ico',
+    '/apple-touch-icon.png',
+    'logo.svg',
+    '/index.html',
+    '/msg-bubble.png',
+    '/logo.png',
+    '/e2ee-placeholder.jpg',
+    '/multidevice-gif.gif',
+    '/instant-messaging.gif',
+    '/attachment-menu.png'
+    // Add other static assets here
+];
+
+// Install the Service Worker
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Opened cache');
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+// Fetch resources from the cache or network
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+// Update the Service Worker and clear old caches
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,10 +1,29 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path'
+
 
 
 export default defineConfig({
     plugins: [ react() ],
     server: {
         port: 3000
+    },
+    build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            worker: resolve(__dirname, 'src/service-worker.js'),
+          },
+          output: {
+            entryFileNames: assetInfo => {
+              // output popup-related files normally
+              if (assetInfo.name === 'worker') return 'service-worker.js'
+              return 'assets/[name]-[hash].js'
+            }
+          }
+        },
+      emptyOutDir: true
     }
+
 });
