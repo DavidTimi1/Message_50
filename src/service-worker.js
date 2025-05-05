34fac49919c44fc66d7fa88638b341dc39416057
David <duwagbale07@gmail.com>
2025-05-05 11:55:12 +0100

    Update (service worker): Add glob paths, offline fallback html

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,46 +1,86 @@
 const CACHE_NAME = 'message50-cache-v1';
-const urlsToCache = [
-    '/',
-    '/app',
-    '/app/contacts',
-    '/app/media',
-    '/app/settings',
-    '/manifest.json',
-    '/favicon.ico',
-    '/apple-touch-icon.png',
-    'logo.svg',
-    '/msg-bubble.png',
-    '/logo.png',
-    '/e2ee-placeholder.jpg',
-    '/multidevice-gif.gif',
-    '/instant-messaging.gif',
-    '/attachment-menu.png'
-    // Add other static assets here
-];
 
+const staticAssets = Object.keys(
+    import.meta.glob([
+      '/index.html',
+      'offlne.html',
+      '/app/index.html',
+      '/login/index.html',
+      '/register/index.html',
+      '/assets/**/*.js',
+      '/assets/**/*.css',
+      '/assets/**/*.{woff2,png,jpg,svg,ico}',
+    ], { as: 'url' })
+  );
+
+  
 // Install the Service Worker
 self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            console.log('Opened cache');
-            return cache.addAll(urlsToCache);
+            return cache.addAll(staticAssets);
         })
     );
 });
 
-// Fetch resources from the cache or network
+
 self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+    // We only want to call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    if (event.request.mode === 'navigate') {
+      event.respondWith((async () => {
+        try {
+            // First, try to use the navigation preload response if it's supported.
+            const preloadResponse = await event.preloadResponse;
+            if (preloadResponse) {
+                return preloadResponse;
+            }
+
+            // Handle static asset requests
+            caches.match(event.request).then((cachedResponse) => {
+                return (
+                    cachedResponse ||
+
+                    fetch(event.request).then( response => {
+                        // update cache
+                        return caches.open(CACHE_NAME).then((cache) => {
+                            cache.put(event.request, response.clone());
+                            return response;
+                        });
+                    })
+                );
+            })
+
+        } catch (error) {
+          // catch is only triggered if an exception is thrown, which is likely
+          // due to a network error.
+          // If fetch() returns a valid HTTP response with a response code in
+          // the 4xx or 5xx range, the catch() will NOT be called.
+          console.log('Fetch failed; returning offline page instead.', error);
+  
+          const cache = await caches.open(CACHE_NAME);
+          const cachedResponse = await cache.match(OFFLINE_URL);
+          return cachedResponse;
+        }
+      })());
+    }
+
+    // If our if() condition is false, then this fetch handler won't intercept the
+    // request. If there are any other fetch handlers registered, they will get a
+    // chance to call event.respondWith(). If no fetch handlers call
+    // event.respondWith(), the request will be handled by the browser as if there
+    // were no service worker involvement.
 });
 
 // Update the Service Worker and clear old caches
 self.addEventListener('activate', (event) => {
     const cacheWhitelist = [CACHE_NAME];
-    event.waitUntil(
+
+    event.waitUntil( async _ => {
+        if ('navigationPreload' in self.registration) {
+          await self.registration.navigationPreload.enable();
+        };
+
         caches.keys().then((cacheNames) => {
             return Promise.all(
                 cacheNames.map((cacheName) => {
@@ -50,5 +90,6 @@ self.addEventListener('activate', (event) => {
                 })
             );
         })
+        }
     );
 });
