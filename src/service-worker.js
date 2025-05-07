8d9ac9ea74c35f05bdd016a11e117aa2c905aa78
David <duwagbale07@gmail.com>
2025-05-07 01:27:06 +0100

    Fix (sw): Removeimport, - Use packages .vite.manifest to get prebuilt files mapping

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,4 +1,5 @@
 const CACHE_NAME = 'message50-cache-v1';
+const OFFLINE_URL = '/offline.html';
 
 
 // Fetch the manifest and cache the assets
@@ -6,11 +7,12 @@ async function cacheAssets() {
     const cache = await caches.open(CACHE_NAME);
 
     // Fetch the manifest.json file
-    const response = await fetch('/manifest.json');
+    const response = await fetch('/.vite/manifest.json');
     const manifest = await response.json();
+    console.log(manifest)
 
     // Extract the asset URLs from the manifest
-    const assetsToCache = Object.values(manifest).map(entry => entry.file);
+    const assetsToCache = Object.values(manifest).map(entry => entry.file === 'service-worker.js'? '' : entry.file);
 
     // Add additional static assets (e.g., HTML files)
     const staticAssets = [
@@ -19,9 +21,12 @@ async function cacheAssets() {
         '/login.html',
         '/register.html',
         '/app.html',
-        '/offline.html',
+        '/offline.html', // included offline file
         '/favicon.ico',
+        '/apple-touch-icon.png',
+        'logo.svg',
         '/logo.png',
+        '/msg-bubble.png',
         '/user-icon.svg',
         'manifest.json'
     ];
@@ -44,61 +49,67 @@ self.addEventListener('install', (event) => {
 self.addEventListener('fetch', (event) => {
     // We only want to call event.respondWith() if this is a navigation request
     // for an HTML page.
+    const {request} = event;
+    
+    event.respondWith((async () => {
+        if (request.mode === 'navigate') {
+            try {
+                // First, try to use the navigation preload response if it's supported.
+                const preloadResponse = await event.preloadResponse;
+                if (preloadResponse) {
+                    return preloadResponse;
+                }
+                
+                const networkResponse = await fetch(request);
+                const cache = await caches.open(CACHE_NAME);
+                cache.put(request, networkResponse.clone());
+                return networkResponse;
 
-    if (event.request.mode === 'navigate') {
-      event.respondWith((async () => {
-        try {
-            // First, try to use the navigation preload response if it's supported.
-            const preloadResponse = await event.preloadResponse;
-            if (preloadResponse) {
-                return preloadResponse;
+            } catch (error) {
+                // catch is only triggered if an exception is thrown, which is likely
+                // due to a network error.
+                // If fetch() returns a valid HTTP response with a response code in
+                // the 4xx or 5xx range, the catch() will NOT be called.
+        
+                const cache = await caches.open(CACHE_NAME);
+                const cachedResponse = await caches.match(request);
+                const offlineView = await cache.match(OFFLINE_URL);
+
+                return cachedResponse ?? offlineView;
             }
-            
-            const networkResponse = await fetch(request);
+        }
+        
+        // If no fetch handlers call event.respondWith(), 
+        // the request will be handled by the browser as if there
+        // were no service worker involvement.
+        
+        // Cache First for static assets (JS, CSS, images, etc.)
+        const cached = await caches.match(request);
+        if (cached) {
+            return cached;
+        }
+
+        try {
+            const response = await fetch(request);
             const cache = await caches.open(CACHE_NAME);
-            cache.put(request, networkResponse.clone());
-            return networkResponse;
+            cache.put(request, response.clone());
+            return response;
 
         } catch (error) {
-          // catch is only triggered if an exception is thrown, which is likely
-          // due to a network error.
-          // If fetch() returns a valid HTTP response with a response code in
-          // the 4xx or 5xx range, the catch() will NOT be called.
-  
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await caches.match(event.request);
-          const offlineView = await cache.match(OFFLINE_URL);
-
-          return cachedResponse ?? offlineView;
+            console.error('Fetch failed; returning offline page instead.', error);
+            const cache = await caches.open(CACHE_NAME);
+            return cache.match(OFFLINE_URL);
         }
-      })());
-    }
 
-    // If no fetch handlers call event.respondWith(), 
-    // the request will be handled by the browser as if there
-    // were no service worker involvement.
-    
-    // Cache First for static assets (JS, CSS, images, etc.)
-    event.respondWith(
-        caches.match(request).then((cached) => {
-        return (
-            cached ||
-            fetch(request).then((response) => {
-            return caches.open(CACHE_NAME).then(async (cache) => {
-                cache.put(request, response.clone());
-                return response;
-            });
-            })
-        );
-        })
-    );
+    })())
 });
+
 
 // Update the Service Worker and clear old caches
 self.addEventListener('activate', (event) => {
     const cacheWhitelist = [CACHE_NAME];
 
-    event.waitUntil( async _ => {
+    event.waitUntil( (async _ => {
         if ('navigationPreload' in self.registration) {
           await self.registration.navigationPreload.enable();
         };
@@ -112,6 +123,5 @@ self.addEventListener('activate', (event) => {
                 })
             );
         })
-        }
-    );
+    })());
 });
